3d85d1a32eb4ef0a951721f52c582501f2fcd1e0
liuzijie <liu.zijie@cesgroup.com.cn>
2019-03-12 21:24:41 +0800

    去除redis

--- src/main/java/com/nettop/demo/service/UserMemberServiceImpl.java
+++ src/main/java/com/nettop/demo/service/UserMemberServiceImpl.java
@@ -2,7 +2,6 @@ package com.nettop.demo.service;
 
 import com.nettop.demo.dao.UserMapper;
 import com.nettop.demo.entity.User;
-import com.nettop.utils.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,12 +13,8 @@ public class UserMemberServiceImpl implements UserMemberService {
     @Resource
     private UserMapper userMapper;
 
-    @Autowired
-    private RedisUtil redisUtil;
 
     public User getUserById(String userId) {
-        Object object = redisUtil.get("lzj");
-        System.out.println(object);
         return userMapper.selectByPrimaryKey(userId);
     }
 
