a6d374c600d5c1549257f5b2341b1b09992376e5
liuzijie <liu.zijie@cesgroup.com.cn>
2019-03-14 21:04:23 +0800

    上传下载功能

--- src/main/java/com/nettop/demo/controller/UserMemberController.java
+++ src/main/java/com/nettop/demo/controller/UserMemberController.java
@@ -5,14 +5,18 @@ import com.nettop.demo.entity.PageEntity;
 import com.nettop.demo.entity.User;
 import com.nettop.demo.service.UserMemberService;
 import com.nettop.utils.PageUtils;
+import com.nettop.utils.UploadUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -25,6 +29,9 @@ import java.util.Map;
 public class UserMemberController {
     @Resource
     private UserMemberService userService;
+
+    @Value("${filePath}")
+    String filePath;
 
     @RequestMapping("/showUser")
     @ResponseBody
@@ -44,4 +51,29 @@ public class UserMemberController {
         result = PageUtils.getPageResult(list);
         return result;
     }
+
+    @RequestMapping(value="/onlineupload", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<String,String> onlineupload(@RequestParam("onlinefilename") MultipartFile file,
+                                           HttpServletRequest request) {
+//        String contentType = file.getContentType();
+
+        Map<String,String> map=new HashMap<>();
+        String fileName = file.getOriginalFilename();
+        System.out.println("fileName"+fileName);
+        System.out.println("filePath        "+filePath);
+        try {
+            UploadUtils.uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        System.out.println("file + success!");
+        if(file==null){
+            map.put("error","false");
+        }
+        map.put("success","true");
+        return map;
+    }
+
 }
